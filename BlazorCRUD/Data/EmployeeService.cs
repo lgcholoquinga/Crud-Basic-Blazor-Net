c25544599b20bd24f82aad7fb7b934063495f6fe
lgcholoquinga <ldu-aries.lk@hotmail.es>
2020-04-24 13:06:41 -0500

    Crud basico de empleados fnalizado

---
+++ BlazorCRUD/Data/EmployeeService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _db;
+        public EmployeeService(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        //cruds Employees
+        //get All Employees
+        public List<EmployeeInfo> getAllEmployee()
+        {
+            var empList = _db.Employees.ToList();
+            return empList;
+        }
+        //create a Employee
+        public string CreateEmployee(EmployeeInfo objEmployee)
+        {
+            _db.Employees.Add(objEmployee);
+            _db.SaveChanges();
+            return "Employee Created Successfull";
+        }
+
+        //get employee by Id
+        public EmployeeInfo getEmployeeById(int id)
+        {
+            EmployeeInfo employee = _db.Employees.FirstOrDefault(s => s.EmployeeId == id);
+            return employee;
+        }
+        //update Employee
+        public string updateEmployee(EmployeeInfo objEmployee)
+        {
+            _db.Employees.Update(objEmployee);
+            _db.SaveChanges();
+            return "Employee Updated Succsessfull";
+        }
+        //delete Employee
+        public string deleteEmployee(EmployeeInfo employee)
+        {
+            _db.Remove(employee);
+            _db.SaveChanges();
+            return "Employee Deleted Successfull";
+        }
+    }
+}
--- BlazorCRUD/Startup.cs
+++ BlazorCRUD/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorCRUD.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorCRUD
 {
@@ -28,7 +29,9 @@ namespace BlazorCRUD
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped<EmployeeService>();
             services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
